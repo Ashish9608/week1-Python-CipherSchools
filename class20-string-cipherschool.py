c21ff9aebb856ae593a52d2be425e819817a2030
Ashish <114793959+Ashish9608@users.noreply.github.com>
2022-12-08 19:42:55 +0530

    Add files via upload

---
+++ class20-string-cipherschool.py
@@ -0,0 +1,14 @@
+first='Ayush'
+second='sharma'
+full= first + second
+print(full)
+full= first +' '+ second
+print(full)
+
+a='Ayush'
+b='3'
+print(a+b)
+b=3
+print(a+str(b))
+
+print(a*3)
---
+++ class30-step.argument-cipherschool.py
@@ -0,0 +1,14 @@
+l1='programme'
+print(l1[2:5]) #stop-1 
+print(l1[0:5]) #progr
+
+print(l1[0:5:2]) #por   
+   
+print(l1[0::2]) #porme
+
+
+#reverse
+print(l1[6::-1])
+
+print(l1[-1::-1]) #emmargorp
+print(l1[::-1])
